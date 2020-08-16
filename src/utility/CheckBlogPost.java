273c4c17cfdcafa043d69388fcbdd9a024f2e544
chaitanya <saichaitanya15236@gmail.com>
2020-08-16 17:14:19 +0530

    FSJ0620D107

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,77 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface
+{
+	String[] offensive_words= {"2 girls 1 cup",
+	                          "2g1c",
+	                          "4r5e",
+	                          "5h1t",
+	                         " 5hit",
+	                          "a$$",
+	                          "a$$hole",
+	                          "a_s_s",
+	                         " a2m",
+	                         " a54",
+	                          "a55",
+	                          "a55hole",
+	                         " acrotomophilia",
+	                          "aeolus",
+	                         " ahole",
+	                         " alabama hot pocket",
+	                          "alaskan pipeline",
+	                          "anal",
+	                         " anal impaler",
+	                          "anal leakage",
+	                          "analprobe",
+	                          "anilingus",
+	                          "anus",
+	                         " apeshit",
+	                          "ar5e",
+	                          "areola",
+	                         " areole",
+	                         " arian",
+	                        "  arrse",
+	                          "arse",
+	                         " arsehole",
+	                         " aryan",
+	                         " ass",
+	                         " ass" ,"fuck"};
+	
+	public boolean checkBlog(Blog blog)
+	{
+		if(checkBlogTitle(blog) && checkBlogDescription(blog))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		String title=blog.getBlogTitle();
+		for(int i=0;i<offensive_words.length;i++)
+		{
+			if(offensive_words[i].equals(title))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		String description=blog.getBlogDescription();
+		for(int i=0;i<offensive_words.length;i++)
+		{
+			if(offensive_words[i].equals(description))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
--- src/utility/OffensiveWordsInterface.java
+++ src/utility/OffensiveWordsInterface.java
@@ -1,1 +1,23 @@
 package utility;
+
+import model.Blog;
+
+public interface OffensiveWordsInterface
+{
+	boolean checkBlogTitle(Blog blog);
+	boolean checkBlogDescription(Blog blog);
+}
+ 
+	
+	
+	                        
+	                        
+	                       
+	                        
+	              
+ 
+ 
+ 
+	 
+ 
+
